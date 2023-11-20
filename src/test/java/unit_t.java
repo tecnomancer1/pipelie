5338fb43e33425c83194f21f823d7558c634f578
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-20 19:54:30 +0000

    Create unit_t.java

---
+++ src/test/java/unit_t.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyUnitTest {
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
