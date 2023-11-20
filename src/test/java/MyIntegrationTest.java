ad6cb407ef80799e01b0f1c408ee941e7a39ba17
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-20 20:20:32 +0000

    Update MyIntegrationTest.java

--- src/test/java/MyIntegrationTest.java
+++ src/test/java/MyIntegrationTest.java
@@ -1,10 +1,24 @@
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MyIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
+
     @Test
-    public void testDatabaseConnection() {
-        // Test database connection and assert true if successful
-        assertTrue(Database.connect());
+    public void testHelloWorldEndpoint() {
+        // Make an HTTP GET request to the /hello endpoint
+        String response = restTemplate.getForObject("http://localhost:" + port + "/hello", String.class);
+
+        // Assert that the response is "Hello, World!"
+        assertEquals("Hello, World!", response);
     }
 }
