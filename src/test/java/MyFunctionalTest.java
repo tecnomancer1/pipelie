537d9f8570ce67f9022d173c72aac7a2e287a1ec
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-20 20:33:32 +0000

    Update MyFunctionalTest.java

--- src/test/java/MyFunctionalTest.java
+++ src/test/java/MyFunctionalTest.java
@@ -1,17 +1,32 @@
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-public class MyFunctionalTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloWorldFunctionalTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
+
     @Test
-    public void testLoginPage() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://example.com/login");
-        driver.findElement(By.id("username")).sendKeys("testuser");
-        driver.findElement(By.id("password")).sendKeys("testpassword");
-        driver.findElement(By.id("loginButton")).click();
-        // Add assertions based on expected behavior
-        driver.quit();
+    public void testHelloWorldEndpoint() {
+        // Make an HTTP GET request to the root endpoint
+        String response = restTemplate.getForObject(getBaseUrl() + "/", String.class);
+
+        // Assert that the response is "Hello World!"
+        assertEquals("Hello World!", response);
+    }
+
+    private String getBaseUrl() {
+        return "http://localhost:" + port + contextPath;
     }
 }
