8ababb82db586f6c58494d769e46e353760733e5
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-21 21:42:59 +0000

    Update and rename HelloWorldFunctionalTest.java to HelloWorldFunctional.java

--- src/test/java/HelloWorldFunctional.java
+++ src/test/java/HelloWorldFunctional.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HelloWorldFunctionalTest {
+public class HelloWorldFunctional {
 
     private static WebDriver driver;
 
