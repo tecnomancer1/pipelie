fca32b962feabed45516f8ae6601f2fb6a98c81b
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-22 19:53:20 +0000

    Update HelloWorldFunctional.java

--- src/test/java/HelloWorldFunctional.java
+++ src/test/java/HelloWorldFunctional.java
@@ -14,7 +14,7 @@ public class HelloWorldFunctional {
     @BeforeAll
     public static void setUp() {
         // Set up WebDriver (assuming ChromeDriver)
-       System.setProperty("webdriver.chrome.driver", "///usr///local///bin///chromedriver");
+       System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
       // driver = new ChromeDriver();
        WebDriver driver= new ChromeDriver(); driver.get("https://www.google.com");
     }
