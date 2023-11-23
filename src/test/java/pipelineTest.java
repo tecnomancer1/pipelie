a3cfc4be55a0a0bf2db635ef83cd86bd038d2612
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-23 22:02:13 +0000

    Update and rename HelloWorldControllerTest.java to pipelineTest.java

--- src/test/java/pipelineTest.java
+++ src/test/java/pipelineTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class HelloWorldControllerTest {
+public class pipelineTest {
 
     @Test
     public void helloWorldEndpointShouldReturnHelloWorld() {
