4699e00c533caf5d97c29c113d06794ed07cfe34
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-23 22:14:34 +0000

    Update pipelineTest.java

--- src/test/java/pipelineTest.java
+++ src/test/java/pipelineTest.java
@@ -1,13 +1,22 @@
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import static org.junit.Assert.assertEquals;
 
-public class pipelineTest {
+public class PipelineTest {
 
     @Test
-    public void helloWorldEndpointShouldReturnHelloWorld() {
-        pipeline application = new pipeline();
-        HelloWorldController controller = new HelloWorldController();
-        String result = controller.helloWorld();
-        assertEquals("Hello World!", result);
+    public void testHelloWorldEndpoint() throws Exception {
+        HelloWorldController helloWorldController = new HelloWorldController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
+
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andReturn().getResponse();
+
+        assertEquals("Hello World!", response.getContentAsString());
+        assertEquals(200, response.getStatus());
     }
 }
