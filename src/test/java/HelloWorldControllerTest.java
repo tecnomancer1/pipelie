542b7193988ac3e82945ab5ffb3e57ec6929e97e
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-23 21:44:00 +0000

    Update HelloWorldControllerTest.java

--- src/test/java/HelloWorldControllerTest.java
+++ src/test/java/HelloWorldControllerTest.java
@@ -1,25 +1,12 @@
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class HelloWorldControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
-    public void helloWorldEndpointShouldReturnHelloWorld() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
-               .andExpect(MockMvcResultMatchers.status().isOk())
-               .andExpect(MockMvcResultMatchers.content().string("Hello World!"));
+    public void helloWorldEndpointShouldReturnHelloWorld() {
+        HelloWorldController controller = new HelloWorldController();
+        String result = controller.helloWorld();
+        assertEquals("Hello World!", result);
     }
 }
