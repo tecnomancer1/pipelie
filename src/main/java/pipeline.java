de659f9dcfff3a2411cec250c9a43c09db67f3d7
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-25 15:13:58 +0000

    Update pipeline.java

--- src/main/java/pipeline.java
+++ src/main/java/pipeline.java
@@ -1,21 +1,19 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
 public class pipeline {
 
-    public static void main(String[] args) {
-        SpringApplication.run(pipeline.class, args);
-    }
-}
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-@RestController
-class HelloWorldController {
-
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World!";
-    }
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
