d88d2f89426064e5fbc7f8ce6a91050837f90d91
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-25 18:42:17 +0000

    Update NewUnitTest.java

--- src/test/java/NewUnitTest.java
+++ src/test/java/NewUnitTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class NewUnitTest {
 
 	@Test
 	void contextLoads() {
