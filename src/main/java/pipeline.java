6a6c7e6d35fbf9d58da914b29e05c8c19b5fc166
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-25 13:52:49 +0000

    Update pipeline.java

--- src/main/java/pipeline.java
+++ src/main/java/pipeline.java
@@ -1,7 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,29 +12,10 @@ public class pipeline {
 }
 
 @RestController
-class CalculatorController {
+class HelloWorldController {
 
-    @GetMapping("/add")
-    public String add(@RequestParam int num1, @RequestParam int num2) {
-        return String.valueOf(num1 + num2);
-    }
-
-    @GetMapping("/subtract")
-    public String subtract(@RequestParam int num1, @RequestParam int num2) {
-        return String.valueOf(num1 - num2);
-    }
-
-    @GetMapping("/multiply")
-    public String multiply(@RequestParam int num1, @RequestParam int num2) {
-        return String.valueOf(num1 * num2);
-    }
-
-    @GetMapping("/divide")
-    public String divide(@RequestParam int num1, @RequestParam int num2) {
-        if (num2 != 0) {
-            return String.valueOf((double) num1 / num2);
-        } else {
-            return "Cannot divide by zero!";
-        }
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
     }
 }
