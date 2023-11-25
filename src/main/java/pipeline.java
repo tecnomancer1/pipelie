c85e08f8e1a7688bfd8ed06e718ab8a85ec08608
tecnomancer1 <114069020+tecnomancer1@users.noreply.github.com>
2023-11-25 13:38:26 +0000

    Update pipeline.java

--- src/main/java/pipeline.java
+++ src/main/java/pipeline.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class pipeline {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalculatorApplication.class, args);
+        SpringApplication.run(pipeline.class, args);
     }
 }
 
